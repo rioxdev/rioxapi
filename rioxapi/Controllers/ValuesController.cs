62909530756f5e89944a798fc9ed434714f06bd5
henrio randriasoa <rioxdev@gmail.com>
2022-12-29 19:27:34 +0100

    Modif pour tester le nouvel agent sur egaming

--- rioxapi/Controllers/ValuesController.cs
+++ rioxapi/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace rioxapi.Controllers
         [HttpGet]
         public ActionResult Get()
         {
-            return Ok("rampage extrem!");
+            return Ok("rampage extreme! 2023 !");
         }
     }
 }
